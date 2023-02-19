a4fc61717da146df7aaff050cde28b0fe6325149
Rifle <rayfal@bk.ru>
2023-02-19 16:32:47 +0300

    add Task11

---
+++ Task11/Program.cs
@@ -0,0 +1,14 @@
+﻿int number = new Random().Next(100, 1000);
+Console.WriteLine($"Cлучайное число из отрезка 100 - 999 => {number}");
+int gluingDigit = GluingDigit(number);
+Console.WriteLine(gluingDigit);
+int GluingDigit(int num)
+{
+    // int firstDigit = num / 100 * 10;
+    // int secondDigit = num % 10;
+    // int result = firstDigit + secondDigit;
+    // return result;
+    int result = (num / 100 * 10) + (num % 10);
+    return result;
+
+}
